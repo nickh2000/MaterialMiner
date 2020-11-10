834a59c93cfa862bdacff47fa5ba3273e63f740e
Nick Hurley <nickhurley2000@gmail.com>
2020-11-09 19:35:57 -0500

    Downloaded the rest of the MatProj Database

--- MaterialsCollector.py
+++ MaterialsCollector.py
@@ -8,7 +8,13 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import periodictable 
 import json
-
+from bokeh.io import output_file, show
+from bokeh.plotting import figure
+from bokeh.sampledata.periodic_table import elements
+from bokeh.transform import dodge, linear_cmap, log_cmap
+import bokeh.palettes
+from bokeh.models import ColorBar
+import math
 
 test_upper_gap = 1.3
 test_lower_gap = -1.1
@@ -70,7 +76,7 @@ def compile_dos():
 		chunk = []
 
 		#iterate through each material ID
-		for i, ID in enumerate(materials[41001:]):
+		for i, ID in enumerate(materials[69001:]):
 			ID = ID.rstrip()
 
 			#Get the DOS data for that material
@@ -159,7 +165,7 @@ def store_candidates():
 		reader = pd.read_csv('dos.csv', chunksize=chunksize)
 
 		#process each chunk
-		for c, chunk in enumerate(itertools.islice(reader, 10)):
+		for c, chunk in enumerate(reader):
 			#process each data entry in a chunk (a pd.Series row object)
 			for r, (index, row) in enumerate(chunk.iterrows()):
 				dos = DosData(row)
@@ -223,7 +229,100 @@ def elements_in_candidates():
 
 	json.dump(elementDict, open('elements.json', 'w+'), indent = 4)
 
+
+def plot_periodic_table():
+	output_file("periodic.html")
+
+	occurences = json.load(open('elements.json', 'r'))
+
+	periods = ["I", "II", "III", "IV", "V", "VI", "VII"]
+	groups = [str(x) for x in range(1, 19)]
+
+	df = elements.copy()
+	df["atomic mass"] = df["atomic mass"].astype(str)
+	df["group"] = df["group"].astype(str)
+	df["period"] = [periods[x-1] for x in df.period]
+	df = df[df.group != "-"]
+	df = df[df.symbol != "Lr"]
+	df = df[df.symbol != "Lu"]
+	df['occurences'] = [f'{round(occurences[x]/occurences["NumCandidates"] * 100, 2)}%' for x in df.symbol]
+	df['colors'] = [math.log(occurences[x] / 2 + 1) for x in df.symbol]
+	
+	p = figure(title="Periodic Table (omitting LA and AC Series)", plot_width=1000, plot_height=450,
+	           x_range=groups, y_range=list(reversed(periods)))
+
+	#Use the field name of the column source
+	mapper = linear_cmap(field_name='colors', palette=bokeh.palettes.Spectral6, low = 1, high = max(df.colors))
+
+	r = p.rect("group", "period", 0.95, 0.95, source=df, fill_alpha=0.6,
+	           color=mapper)
+
+	color_bar = ColorBar(color_mapper=mapper['transform'], width=8,  location=(0,0))
+
+	p.add_layout(color_bar, 'right')
+
+
+	text_props = {"source": df, "text_align": "left", "text_baseline": "middle"}
+
+	x = dodge("group", -0.4, range=p.x_range)
+
+	p.text(x=x, y="period", text="symbol", text_font_style="bold", **text_props)
+
+	p.text(x=x, y=dodge("period", 0.3, range=p.y_range), text="atomic number",
+	       text_font_size="11px", **text_props)
+
+	p.text(x=x, y=dodge("period", -0.35, range=p.y_range), text="name",
+	       text_font_size="7px", **text_props)
+
+	p.text(x=x, y=dodge("period", -0.2, range=p.y_range), text="occurences",
+	       text_font_size="7px", **text_props)
+
+	p.text(x=["3", "3"], y=["VI", "VII"], text=["LA", "AC"], text_align="center", text_baseline="middle")
+
+	p.outline_line_color = None
+	p.grid.grid_line_color = None
+	p.axis.axis_line_color = None
+	p.axis.major_tick_line_color = None
+	p.axis.major_label_standoff = 0
+	p.legend.orientation = "horizontal"
+	p.legend.location ="top_center"
+	p.hover.renderers = [r] # only hover element boxes
+
+	show(p)
+
+def clean_dos():
+	with open('database.txt') as d:
+
+		reader = pd.read_csv('dos.csv', chunksize = 5000)
+
+		materials = [m.rstrip() for m in d.readlines()]
+		missing = {m: True for m in materials}
+		
+
+		for chunk in reader:
+			for i, material in enumerate(materials):
+
+				if missing[material]:
+					missing[material] = not (material in list(chunk['ID']))
+		
+		missingMaterials = []
+		for k, v in missing.items():
+			if v:
+
+				r = requests.get(f'https://www.materialsproject.org/rest/v2/materials/{material}/vasp/dos?API_KEY={API_KEY}')
+				r = r.json()['response'][0]['dos']
+
+				#get different properties of the DOS array
+				energies =list(r['energies'])
+				densities = list(r['densities']['1'])
+				efermi = r['efermi']
+
+				missingMaterials.append([material, energies, densities, efermi])
+
+
+		df = pd.DataFrame(missingMaterials, columns = ['ID', 'Energies', 'Densities', 'efermi'])
+		df.to_csv('dos.csv', mode='a', header=False)
+
+
 if __name__ == '__main__':
-	elements = json.load(open('elements.json', 'r'))
-	sorted_elements = sorted(elements.items(), key = lambda x: x[1], reverse = True)
-	print(sorted_elements)
+	plot_periodic_table()
